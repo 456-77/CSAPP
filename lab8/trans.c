93304fb9b22dbeeb8785817ceec0319e2788306a
shuiyi <134250097+456-77@users.noreply.github.com>
2024-11-27 18:40:22 +0800

    new file:   lab8/IMG_0532(20241127-183625).PNG 	new file:   lab8/IMG_0533.JPG 	new file:   lab8/IMG_0534(20241127-183715).PNG 	modified:   lab8/notes.md 	modified:   lab8/trans.c
    Changes not staged for commit:
    	modified:   lab8/notes.md

--- lab8/trans.c
+++ lab8/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - 矩阵转置 B = A^T
  *
  * 每个转置函数必须具有如下形式的原型：
@@ -6,51 +6,111 @@
  *
  * 转置函数的评价标准是计算在一个 1KB 直接映射缓存中，
  * 使用 32 字节的块大小时的缓存未命中次数。
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - 这是作业 Part B 中将被评分的转置函数。
  * 请不要修改描述字符串 "Transpose submission"，因为驱动程序会
  * 搜索该字符串来识别需要评分的转置函数。
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{ 
-    int temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8;
-    for(int i;i<N;i++)
-    for(int j=0;j<M;j=j+8)
-    for(int k=i;k<i+8;k++)
-    {
-        temp1=A[k][j];
-        temp2=A[k][j+1];
-        temp3=A[k][j+2];
-        temp4=A[k][j+3];
-        temp5=A[k][j+4];
-        temp6=A[k][j+5];
-        temp7=A[k][j+6];
-        temp8=A[k][j+7];
-        B[j][k]=temp1;
-        B[j+1][k]=temp2;
-        B[j+2][k]=temp3;
-        B[j+3][k]=temp4;
-        B[j+4][k]=temp5;
-        B[j+5][k]=temp6;
-        B[j+6][k]=temp7;
-        B[j+7][k]=temp8;
-    
-
-    }
+{
+    int temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8;
+    for (int i; i < N; i++)
+        for (int j = 0; j < M; j = j + 8)
+            for (int k = i; k < i + 8; k++)
+            {
+                temp1 = A[k][j];
+                temp2 = A[k][j + 1];
+                temp3 = A[k][j + 2];
+                temp4 = A[k][j + 3];
+                temp5 = A[k][j + 4];
+                temp6 = A[k][j + 5];
+                temp7 = A[k][j + 6];
+                temp8 = A[k][j + 7];
+                B[j][k] = temp1;
+                B[j + 1][k] = temp2;
+                B[j + 2][k] = temp3;
+                B[j + 3][k] = temp4;
+                B[j + 4][k] = temp5;
+                B[j + 5][k] = temp6;
+                B[j + 6][k] = temp7;
+                B[j + 7][k] = temp8;
+            }
 }
-
-/* 
+// 针对64*64进行优化
+char transpose_64_desc[] = "Transpose 64";
+void transpose_64(int M, int N, int A[N][M], int B[M][N])
+{
+    int temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8;
+    for (int i = 0; i < M; i += 8)//A,B的行
+        for (int j = 0; j < N; j += 8)//A,B的列
+        {
+            //k用来遍历4*4小块的行,列不需要另设
+            //将A的上面两块倒置并复制到B的上面
+            for(int k=i;k<i+4;k++)
+            {
+                //获取一行A
+                temp1=A[k][j+0];//miss其他都hit
+                temp2=A[k][j+1];
+                temp3=A[k][j+2];
+                temp4=A[k][j+3];
+                temp5=A[k][j+4];
+                temp6=A[k][j+5];
+                temp7=A[k][j+6];
+                temp8=A[k][j+7];
+                //复制给B
+                B[j+0][k]=temp1;//这部分位置已经正确,后续不需要修改
+                B[j+1][k]=temp2;
+                B[j+2][k]=temp3;
+                B[j+3][k]=temp4;
+                B[j+0][k+4]=temp5;//后续任然需要修改
+                B[j+1][k+4]=temp6;
+                B[j+2][k+4]=temp7;
+                B[j+3][k+4]=temp8;
+            }
+        }
+        //现在缓存中依然保存有B上面两小块
+        //用本地变量把B的两小块存储下来
+        for(int k=i;K<i+4;k++)
+        {
+            //本地变量存储B的右上部分
+            temp1=B[j+0][k+4];
+            temp2=B[j+1][k+4];
+            temp3=B[j+2][k+4];
+            temp4=B[j+3][k+4];
+            //将A的左下部分倒置并复制给B的右上
+            B[j+0][k+4]=A[k+4][j+0]
+            B[j+1][k+4]=A[k+4][j+1];
+            B[j+2][k+4]=A[k+4][j+2];
+            B[j+3][k+4]=A[k+4][j+3];
+            //利用存储的本地变量,把A的右上复制给B的左下
+            B[k+4][j+0]=temp1;
+            B[k+4][j+1]=temp2;
+            B[k+4][j+2]=temp3;
+            B[k+4][j+3]=temp4;
+        }
+        //现在缓存中存的都是B
+        //将A的右下复制给B的右下
+        for( int k=i ;k<i+4;k++)
+        {
+            B[k+4][j+4+0]=A[k+4][j+4+0];
+            B[k+4][j+4+1]= A[k+4][j+4+1];
+            B[k+4][j+4+2]= A[k+4][j+4+2];
+            B[k+4][j+4+3]= A[k+4][j+4+3];
+        }
+            
+}
+/*
  * 你可以在下面定义额外的转置函数。以下是一个简单的示例函数，
  * 它并未针对缓存进行优化。
- */ 
-/* 
+ */
+/*
  * trans - 一个简单的基线转置函数，按行扫描，未针对缓存进行优化。
  */
 char trans_desc[] = "Simple row-wise scan transpose";
@@ -58,13 +118,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -75,14 +136,13 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* 注册你的解决方案函数 */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* 注册其他任何额外的转置函数 */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
-/* 
+/*
  * is_transpose - 该辅助函数检查 B 是否为 A 的转置。
  * 你可以在转置函数返回之前调用此函数以检查转置的正确性。
  */
@@ -90,13 +150,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
